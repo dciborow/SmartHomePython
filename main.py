6aa51a425af69e9fec6da893af006b4802f93e5b
Daniel Ciborowski <dciborow@microsoft.com>
2021-01-18 23:27:10 -0500

    First Commit

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # This is a sample Python script.
 import json
+import yaml
 
 from smarthome.honeywell import get_devices, get_rooms
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bearer = "1VwAR1waukxuUjSOE5AgNHr3h2OK"
 
     api_key = "v4M5vCGGt0F6tMwxwb2ac5UGENegSS4H"
     location_id = "2231065"
     deviceID = "LCC-48A2E6039C30"
 
-    get_devices_response = get_devices(bearer, api_key, location_id)
-    print(json.dumps(get_devices_response, indent=3))
+    with open("config.yaml") as f:
+        # use safe_load instead load
+        config = yaml.safe_load(f)
 
-    get_rooms_response = get_rooms(bearer, api_key, location_id, deviceID, "0")
-    print(json.dumps(get_rooms_response, indent=3))
+        get_devices_response = get_devices(
+            config["bearer"], config["api_key"], config["location_id"]
+        )
+        print(json.dumps(get_devices_response, indent=3))
+
+        get_rooms_response = get_rooms(
+            config["bearer"],
+            config["api_key"],
+            config["location_id"],
+            config["deviceID"],
+            "0",
+        )
+        print(json.dumps(get_rooms_response, indent=3))
--- smarthome/honeywell.py
+++ smarthome/honeywell.py
@@ -2,6 +2,8 @@ import json
 
 import requests
 
+BASE_URL = "https://api.honeywell.com/v2/"
+
 
 def get_devices(bearer, api_key, location_id):
     """
@@ -14,30 +16,28 @@ def get_devices(bearer, api_key, location_id):
         --header "Authorization: Bearer 1VwAR1waukxuUjSOE5AgNHr3h2OK" \
         "https://api.honeywell.com/v2/devices?apikey=v4M5vCGGt0F6tMwxwb2ac5UGENegSS4H&locationId=2231065"
     """
-    params = {
-        "apikey": api_key,
-        "locationId": location_id
-    }
+    params = {"apikey": api_key, "locationId": location_id}
 
-    response = requests.get(BASE_URL + "devices", params=params, headers={"Authorization": "Bearer " + bearer})
-    if response.status_code != 200:
-        raise BaseException
-
-    return json.loads(response.content.decode())
-
-
-def get_rooms(bearer, api_key, location_id, deviceId, groupId):
-    params = {
-        "apikey": api_key,
-        "locationId": location_id
-    }
-
-    response = requests.get(BASE_URL + "devices/thermostats/" + deviceId + "/group/" + groupId + "/rooms",
-                            params=params, headers={"Authorization": "Bearer " + bearer})
+    response = requests.get(
+        BASE_URL + "devices",
+        params=params,
+        headers={"Authorization": "Bearer " + bearer},
+    )
     if response.status_code != 200:
         raise BaseException(response.status_code)
 
     return json.loads(response.content.decode())
 
 
-BASE_URL = "https://api.honeywell.com/v2/"
+def get_rooms(bearer, api_key, location_id, deviceId, groupId):
+    params = {"apikey": api_key, "locationId": location_id}
+
+    response = requests.get(
+        BASE_URL + "devices/thermostats/" + deviceId + "/group/" + groupId + "/rooms",
+        params=params,
+        headers={"Authorization": "Bearer " + bearer},
+    )
+    if response.status_code != 200:
+        raise BaseException(response.status_code)
+
+    return json.loads(response.content.decode())
